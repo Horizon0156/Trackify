0528521ad5ee77c1689d7a6058a87020bebcb9db
desch <bitbucket@dschiavo.net>
2016-10-02 18:27:18 +0200

    Added converter for coloring TotalEffort green if minimum needed value is reached.

---
+++ BookingHelper/Converter/TotalEffortToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BookingHelper.Converter
+{
+    public class TotalEffortToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type t, object parameter, CultureInfo culture)
+        {
+            var effort = System.Convert.ToDouble(value);
+            var neededEffort = System.Convert.ToDouble(parameter);
+            return (effort >= neededEffort) ? Brushes.Green : Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type t, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
