f90dd33f7fea5518418a68261dde855403b58a2e
Stefan Wilkes <stefan.wilkes@gmail.com>
2016-11-22 19:26:00 +0100

    Cleaned up program structure, introduced messenger, updated initial focus.

---
+++ Horizon.Framework/Services/IMessenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Horizon.Framework.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMessenger
+    {
+        void Send<T>(T message);
+
+        void Register<T>(Action<T> messageHandler);
+
+        void Unregister<T>();
+
+        void Unregister(Type messageType);
+    }
+}
