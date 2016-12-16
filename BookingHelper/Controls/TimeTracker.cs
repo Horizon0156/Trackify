246a3ab5297cee5a79c18f1e31dfd56e0a51d65b
Stefan Wilkes <stefan.wilkes@gmail.com>
2016-12-16 18:26:37 +0100

    Finalized TimeTracker control

--- BookingHelper/Controls/TimeTracker.cs
+++ BookingHelper/Controls/TimeTracker.cs
@@ -1,62 +1,63 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace BookingHelper.Controls
 {
-    using System.Windows.Threading;
-
-    using JetBrains.Annotations;
-
     internal class TimeTracker : TextBlock
     {
-        [NotNull]
-        private DispatcherTimer _updateTimer;
+        public static readonly DependencyProperty EllapsedTimeProperty;
 
-        private bool _intializeTimebase;
+        public static readonly DependencyProperty IsRunningProperty = DependencyProperty.Register(
+            "IsRunning",
+            typeof(bool),
+            typeof(TimeTracker),
+            new FrameworkPropertyMetadata(defaultValue: false, propertyChangedCallback: ToggleTimeTracking));
 
-        public static DependencyProperty EllapsedTimeProperty = DependencyProperty.Register(
+        public static readonly DependencyProperty StartTimeProperty = DependencyProperty.Register(
+            "StartTime",
+            typeof(DateTime?),
+            typeof(TimeTracker),
+            new FrameworkPropertyMetadata(defaultValue: null, propertyChangedCallback: UpdateEllapsedTime));
+
+        public static readonly DependencyProperty StopTimeProperty = DependencyProperty.Register(
+           "StopTime",
+           typeof(DateTime?),
+           typeof(TimeTracker),
+           new FrameworkPropertyMetadata(defaultValue: null, propertyChangedCallback: UpdateEllapsedTime));
+
+        private static readonly DependencyPropertyKey _ellapsedTimePropertyKey = DependencyProperty.RegisterReadOnly(
             "EllapsedTime",
             typeof(TimeSpan),
             typeof(TimeTracker),
             new FrameworkPropertyMetadata(TimeSpan.Zero));
 
-        public static DependencyProperty IsRunningProperty = DependencyProperty.Register(
-            "IsRunning",
-            typeof(bool),
-            typeof(TimeTracker),
-            new FrameworkPropertyMetadata(defaultValue: false, propertyChangedCallback: StartOrStopTimeTracker));
+        private readonly DispatcherTimer _updateTimer;
 
-        private DateTime _startTime;
-
-        private TimeSpan _ellapsedTimeAtStart;
+        static TimeTracker()
+        {
+            EllapsedTimeProperty = _ellapsedTimePropertyKey.DependencyProperty;
+        }
 
         public TimeTracker()
         {
             _updateTimer = new DispatcherTimer();
             _updateTimer.Tick += UpdateEllapsedTime;
             _updateTimer.Interval = TimeSpan.FromSeconds(1);
-            
-            EllapsedTime = TimeSpan.FromSeconds(50);
-            _intializeTimebase = true;
+
             Text = "00:00:00";
         }
 
-        private void UpdateEllapsedTime(object sender, EventArgs e)
+        public TimeSpan EllapsedTime
         {
-            EllapsedTime = _ellapsedTimeAtStart + (DateTime.Now - _startTime);
-
-            if (EllapsedTime.TotalSeconds < 60)
+            get
             {
-                Text = $"{(int) EllapsedTime.TotalSeconds} sec";
+                return (TimeSpan)GetValue(EllapsedTimeProperty);
             }
-            else if (EllapsedTime.TotalMinutes < 60)
+            private set
             {
-                Text = $"{EllapsedTime.ToString("mm\\:ss")} min";
-            }
-            else
-            {
-                Text = EllapsedTime.ToString("hh\\:mm\\:ss");
+                SetValue(_ellapsedTimePropertyKey, value);
             }
         }
 
@@ -72,38 +73,88 @@ namespace BookingHelper.Controls
             }
         }
 
-        public TimeSpan EllapsedTime
+        public DateTime? StartTime
         {
             get
             {
-                return (TimeSpan) GetValue(EllapsedTimeProperty);
+                return (DateTime?)GetValue(StartTimeProperty);
             }
             set
             {
-                SetValue(EllapsedTimeProperty, value);
+                SetValue(StartTimeProperty, value);
             }
         }
 
-        private static void StartOrStopTimeTracker(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public DateTime? StopTime
+        {
+            get
+            {
+                return (DateTime?)GetValue(StopTimeProperty);
+            }
+            set
+            {
+                SetValue(StopTimeProperty, value);
+            }
+        }
+
+        private static void ToggleTimeTracking(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var timeTracker = (TimeTracker)d;
 
-            timeTracker.StartOrStopTimeTracker();
+            timeTracker.ToggleTimeTracking();
         }
 
-        private void StartOrStopTimeTracker()
+        private static void UpdateEllapsedTime(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
+            var timeTracker = (TimeTracker)d;
+            timeTracker.UpdateEllapsedTime((timeTracker.StopTime - timeTracker.StartTime) ?? TimeSpan.Zero);
+        }
+
+        private void ToggleTimeTracking()
+        {
+            _updateTimer.IsEnabled = IsRunning;
+
             if (IsRunning)
             {
-                InitializeTimebase();
+                StartTime = !StartTime.HasValue || StopTime.HasValue
+                    ? DateTime.Now
+                    : StartTime;
+                StopTime = null;
+
+                UpdateEllapsedTime(DateTime.Now - StartTime.Value);
             }
-            _updateTimer.IsEnabled = IsRunning;
+            else
+            {
+                StopTime = DateTime.Now;
+                UpdateEllapsedTime((StopTime - StartTime) ?? TimeSpan.Zero);
+            }
         }
 
-        private void InitializeTimebase()
+        private void UpdateEllapsedTime(object sender, EventArgs e)
         {
-            _startTime = DateTime.Now;
-            _ellapsedTimeAtStart = EllapsedTime;
+            UpdateEllapsedTime((DateTime.Now - StartTime) ?? TimeSpan.Zero);
+        }
+
+        private void UpdateEllapsedTime(TimeSpan ellapsedTime)
+        {
+            EllapsedTime = ellapsedTime;
+
+            if (EllapsedTime.TotalSeconds < 1)
+            {
+                Text = "00:00:00";
+            }
+            else if (EllapsedTime.TotalSeconds < 60)
+            {
+                Text = $"{(int)EllapsedTime.TotalSeconds} sec";
+            }
+            else if (EllapsedTime.TotalMinutes < 60)
+            {
+                Text = $"{EllapsedTime:m\\:ss} min";
+            }
+            else
+            {
+                Text = EllapsedTime.ToString("hh\\:mm\\:ss");
+            }
         }
     }
 }
